9a4080f3e6185abf4a293c1ae62f305a8c900cc0
Mohammed Juzer <85620723+mji10@users.noreply.github.com>
2023-06-12 15:51:30 +0530

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,16 @@
-const navbarToggler = document.querySelector('.navbar--toggler');
-const navbarCollapse = document.querySelector('.main-nav');
+// const navbarToggler = document.querySelector('.navbar--toggler');
+// const navbarCollapse = document.querySelector('.main-nav');
 
-navbarToggler.addEventListener('click', () => {
-    navbarToggler.classList.toggle('change');
+// navbarToggler.addEventListener('click', () => {
+//     navbarToggler.classList.toggle('change');
+//     navbarCollapse.classList.toggle('show__nav');
+// });
+
+
+// Navbar using span
+function openMenu(){
+    const navbarCollapse = document.querySelector('.main-nav');
+    var burger = document.querySelector('.burger_menu');
+    burger.classList.toggle('open');
     navbarCollapse.classList.toggle('show__nav');
-});
+  }
