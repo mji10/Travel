b3b7330e81f76106d0bc8b377dd2ce68b8c16629
Mohammed Juzer <85620723+mji10@users.noreply.github.com>
2023-06-14 16:20:15 +0530

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,19 @@
-// const navbarToggler = document.querySelector('.navbar--toggler');
-// const navbarCollapse = document.querySelector('.main-nav');
-
-// navbarToggler.addEventListener('click', () => {
-//     navbarToggler.classList.toggle('change');
-//     navbarCollapse.classList.toggle('show__nav');
-// });
-
-
-// Navbar using span
-function openMenu(){
-    const navbarCollapse = document.querySelector('.main-nav');
-    var burger = document.querySelector('.burger_menu');
-    burger.classList.toggle('open');
-    navbarCollapse.classList.toggle('show__nav');
+const navbar = document.querySelector('.header');
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add('nav-active');
+  } else {
+    navbar.classList.remove('nav-active');
   }
+};
+
+let btn = document.querySelector('.burger_menu');
+btn.addEventListener('click', () => {
+  const navbarCollapse = document.querySelector('.main-nav');
+  var burger = document.querySelector('.burger_menu');
+  var body = document.querySelector('body')
+  burger.classList.toggle('open');
+  navbarCollapse.classList.toggle('show__nav');
+  body.classList.toggle('no_scroll');
+
+})
